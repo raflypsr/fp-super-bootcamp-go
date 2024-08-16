e73f8a41343313599732e895ab0bac0c2fc1fbdc
rafly <raflynvd16@gmail.com>
2024-08-16 22:39:09 +0700

    membuat deploy vercel

---
+++ api/api.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"fp-super-bootcamp-go/config"
+	"fp-super-bootcamp-go/docs"
+	"fp-super-bootcamp-go/routes"
+	"fp-super-bootcamp-go/utils"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @termsOfService http://swagger.io/terms/
+
+var (
+	App *gin.Engine
+)
+
+func init() {
+	App = gin.New()
+	// for load godotenv
+	// for env
+	environment := utils.Getenv("ENVIRONMENT", "development")
+
+	if environment == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	//programmatically set swagger info
+	docs.SwaggerInfo.Title = "Review Book REST API"
+	docs.SwaggerInfo.Description = "This is REST API Review Book."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = utils.Getenv("HOST", "fp-book-review-app.up.railway.app")
+	docs.SwaggerInfo.Schemes = []string{"https"}
+
+	db := config.ConnectDataBase()
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	r := routes.SetupRouter(db)
+	r.Run()
+}
+
+// Entrypoint
+func Handler(w http.ResponseWriter, r *http.Request) {
+	App.ServeHTTP(w, r)
+}
--- main.go
+++ main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"fp-super-bootcamp-go/config"
-	"fp-super-bootcamp-go/docs"
-	"fp-super-bootcamp-go/routes"
-	"fp-super-bootcamp-go/utils"
-	"log"
-
-	"github.com/joho/godotenv"
+	"fp-super-bootcamp-go/api"
 )
 
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @termsOfService http://swagger.io/terms/
-
 func main() {
-	// for load godotenv
-	// for env
-	environment := utils.Getenv("ENVIRONMENT", "development")
-
-	if environment == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-	}
-
-	//programmatically set swagger info
-	docs.SwaggerInfo.Title = "Review Book REST API"
-	docs.SwaggerInfo.Description = "This is REST API Review Book."
-	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = utils.Getenv("HOST", "fp-book-review-app.up.railway.app")
-	docs.SwaggerInfo.Schemes = []string{"https"}
-
-	db := config.ConnectDataBase()
-	sqlDB, _ := db.DB()
-	defer sqlDB.Close()
-
-	r := routes.SetupRouter(db)
-	r.Run()
-
+	api.App.Run()
 }
