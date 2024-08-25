b2f565f54f15a3ca260774c064de079923a1d9a2
rafly <raflynvd16@gmail.com>
2024-08-25 19:51:22 +0700

    add return

--- api/vercel.go
+++ api/vercel.go
@@ -37,7 +37,7 @@ func init() {
 		docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	} else {
 		docs.SwaggerInfo.Host = os.Getenv("HOST")
-		docs.SwaggerInfo.Schemes = []string{"http"}
+		docs.SwaggerInfo.Schemes = []string{"https"}
 	}
 
 	db := config.ConnectDataBase()
